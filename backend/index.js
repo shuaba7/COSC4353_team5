0d1bb66a9ecc876f73f2e4161574f20721fbbd0c
Christopher Le <le.ctcuong@gmail.com>
2024-03-21 02:57:02 -0500

    finished temporary backend

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+//api methods
+
+var Express = require("express");
+//add database package here
+var cors = require("cors");
+const multer = require("multer");
+
+var app = Express();
+//app.arguments(cors());
+
+console.log("Server starting")
